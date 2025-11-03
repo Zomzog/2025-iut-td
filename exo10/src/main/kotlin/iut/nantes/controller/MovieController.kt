003ea84e9db0f52226821583029a2bb8d092ea18
Zomzog <zomzog@gmail.com>
2025-11-04 00:13:51 +0100

    Exo18

--- exo10/src/main/kotlin/iut/nantes/controller/MovieController.kt
+++ exo10/src/main/kotlin/iut/nantes/controller/MovieController.kt
@@ -1,7 +1,10 @@
 package iut.nantes.controller
 
 import iut.nantes.Database
+import iut.nantes.FR_CA
+import iut.nantes.FR_FR
 import iut.nantes.Movie
+import org.apache.tomcat.util.http.parser.AcceptLanguage
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.DeleteMapping
@@ -10,8 +13,10 @@ import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.PutMapping
 import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RequestHeader
 import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.servlet.function.ServerRequest
 
 @RestController
 class MovieController(val database: Database) {
@@ -25,14 +30,22 @@ class MovieController(val database: Database) {
         }
 
     @GetMapping("/api/movies")
-    fun findAll(@RequestParam(name = "rating") queryRating: List<Int>?,): List<Movie> {
+    fun findAll(@RequestParam(name = "rating") queryRating: List<Int>?,
+                @RequestHeader(name= "Accept-Language", defaultValue = "en-EN") acceptLanguage: String ): List<Movie> {
        val all = database.findAll()
         return if (queryRating != null) {
              all.filter { it.rating in queryRating }
         } else {
             all
-        }
+        }.map { translate(it, acceptLanguage) }
     }
+
+    private fun translate(movie: Movie, acceptLanguage: String) =
+        when (acceptLanguage.uppercase()) {
+            "FR-FR" -> movie.copy(name = FR_FR.get(movie.name) ?: movie.name)
+            "FR-CA" -> movie.copy(name = FR_CA .get(movie.name) ?: movie.name)
+            else -> movie
+        }
 
     @GetMapping("/api/movies/{name}")
     fun getOne(@PathVariable name: String) =
