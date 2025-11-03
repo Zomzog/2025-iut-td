2851ebf46ea9b447d1c439e589819622f65262d9
Zomzog <zomzog@gmail.com>
2025-11-04 00:20:51 +0100

    Exo19

--- exo10/src/test/kotlin/iut/nantes/MovieControllerTestIT.kt
+++ exo10/src/test/kotlin/iut/nantes/MovieControllerTestIT.kt
@@ -5,7 +5,6 @@ import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.http.MediaType
-import org.springframework.http.RequestEntity.post
 import org.springframework.test.web.servlet.MockMvc
 import org.springframework.test.web.servlet.get
 import org.springframework.test.web.servlet.post
@@ -22,18 +21,47 @@ import org.springframework.test.web.servlet.post
  */
 @AutoConfigureMockMvc
 @SpringBootTest
-class MovieControllerTest {
+class MovieControllerTestIT {
 
     @Autowired
     lateinit var mockMvc: MockMvc
 
+    @Autowired
+    lateinit var database: Database
+
     @Test
-    fun demoGet() {
+    fun list() {
+        // GIVEN
+        database.addMovie(Movie("The Dark Knight", 2008, 94, listOf("VO")))
+        // WHEN
        mockMvc.get("/api/movies")
             .andExpect {
+                // THEN
                 status { isOk() }
                 content { contentType("application/json") }
                 jsonPath("$[0].name") { value("The Dark Knight") }
+            }
+    }
+
+    @Test
+    fun add() {
+        // GIVEN
+        // WHEN
+        mockMvc.post("/api/movies")
+        {
+            contentType = MediaType.APPLICATION_JSON
+            content = """{
+                "name": "The Dark Knight",
+                "releaseDate": 2008,
+                "rating": 94,
+                "languages": ["VO"]
+            }"""
+        }
+            .andExpect {
+                // THEN
+                status { isCreated() }
+                content { contentType("application/json") }
+                jsonPath("$.name") { value("The Dark Knight") }
             }
     }
 }
