31edc0e4d2810d3d5247777c9edc1333dfd73aac
Zomzog <zomzog@gmail.com>
2025-11-03 22:57:56 +0100

    Exo0

---
+++ exo1/src/main/kotlin/iut/nantes/ListDatabase.kt
@@ -0,0 +1,35 @@
+package iut.nantes
+
+import java.util.UUID
+
+class ListDatabase: Database{
+    private val users = mutableListOf<User>()
+
+    override fun save(user: User) {
+        users.add(user)
+    }
+
+    override fun delete(user: User) {
+        users.removeIf { it.id == user.id }
+    }
+
+    override fun update(user: User) {
+        val index = users.indexOfFirst { it.id == user.id }
+        if (index != -1) {
+            users[index] = user
+        }
+    }
+
+    override fun findOne(id: UUID): User? {
+        return users.find { it.id == id }
+    }
+
+    override fun findAll(name: String?): List<User> {
+        return if (name == null) {
+            users.toList()
+        } else {
+            users.filter { it.name == name }
+        }
+    }
+
+}
