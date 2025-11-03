d47475afe5e68f41520a0894fc91be790763429b
Zomzog <zomzog@gmail.com>
2025-11-03 23:54:54 +0100

    Exo14

--- exo10/src/main/kotlin/iut/nantes/controller/MovieController.kt
+++ exo10/src/main/kotlin/iut/nantes/controller/MovieController.kt
@@ -5,6 +5,7 @@ import iut.nantes.Movie
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RestController
@@ -22,4 +23,10 @@ class MovieController(val database: Database){
 
     @GetMapping("/api/movies")
     fun findAll() = database.findAll()
+
+    @GetMapping("/api/movies/{name}")
+    fun getOne(@PathVariable name: String) =
+        database.getOne(name)
+            ?.let { ResponseEntity.ok(it) }
+            ?: ResponseEntity.status(HttpStatus.NOT_FOUND).build()
 }
