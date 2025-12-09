6801771addb124dfdeb54291abf08619a137be6e
Zomzog <zomzog@gmail.com>
2025-12-09 16:52:11 +0100

    Exo43

--- exo33/src/main/kotlin/iut/nantes/exo33/config/SecurityConfig.kt
+++ exo33/src/main/kotlin/iut/nantes/exo33/config/SecurityConfig.kt
@@ -8,6 +8,11 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
 import org.springframework.security.web.SecurityFilterChain
 import org.springframework.security.config.annotation.web.invoke
+import org.springframework.security.core.userdetails.User
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
+import org.springframework.security.crypto.password.PasswordEncoder
+import org.springframework.security.provisioning.InMemoryUserDetailsManager
+import org.springframework.security.provisioning.UserDetailsManager
 
 
 @EnableWebSecurity
@@ -26,5 +31,23 @@ class SecurityConfig {
             formLogin { }
         }
         return http.build()
+    }
+
+    @Bean
+    fun passwordEncoder(): PasswordEncoder {
+        return BCryptPasswordEncoder()
+    }
+
+    @Bean
+    fun userDetailService(passwordEncoder: PasswordEncoder): UserDetailsManager {
+        val admin = User.withUsername("admin")
+            .password(passwordEncoder.encode("1234"))
+            .roles("ADMIN")
+            .build()
+        val demo = User.withUsername("login")
+            .password(passwordEncoder.encode("password"))
+            .roles("ADMIN")
+            .build()
+        return InMemoryUserDetailsManager(admin, demo)
     }
 }
