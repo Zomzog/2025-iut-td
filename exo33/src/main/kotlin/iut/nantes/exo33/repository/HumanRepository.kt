d83f350d9c73dbd4701a319196c73e6c490c5d5a
Zomzog <zomzog@gmail.com>
2025-12-09 13:47:48 +0100

    Exo36

--- exo33/src/main/kotlin/iut/nantes/exo33/repository/HumanRepository.kt
+++ exo33/src/main/kotlin/iut/nantes/exo33/repository/HumanRepository.kt
@@ -1,9 +1,13 @@
 package iut.nantes.exo33.repository
 
+import jakarta.persistence.CascadeType
 import jakarta.persistence.Entity
+import jakarta.persistence.FetchType
 import jakarta.persistence.GeneratedValue
 import jakarta.persistence.GenerationType
 import jakarta.persistence.Id
+import jakarta.persistence.JoinColumn
+import jakarta.persistence.OneToOne
 import org.springframework.data.jpa.repository.JpaRepository
 
 interface HumanRepository: JpaRepository<HumanEntity, Int>
@@ -12,4 +16,14 @@ interface HumanRepository: JpaRepository<HumanEntity, Int>
 data class HumanEntity(
     @Id @GeneratedValue(strategy = GenerationType.AUTO)
     val humanId: Int?,
-    val name: String)
+    val name: String,
+    @OneToOne(cascade = [(CascadeType.ALL)])
+    @JoinColumn(name = "contact_id")
+    val contactEntity: ContactEntity
+    )
+
+@Entity
+data class ContactEntity(
+    @Id @GeneratedValue(strategy = GenerationType.AUTO)
+    val contactId: Int?,
+    val email: String)
