aecd8ae7bab5a64ba9b6c105d52f3ae0adc7689e
Zomzog <zomzog@gmail.com>
2025-12-09 13:10:44 +0100

    Exo26

--- exo21/src/main/kotlin/iut/nantes/exo21/config/ErrorHandler.kt
+++ exo21/src/main/kotlin/iut/nantes/exo21/config/ErrorHandler.kt
@@ -2,9 +2,13 @@ package iut.nantes.exo21.config
 
 import iut.nantes.exo21.errors.ImATeapotException
 import jakarta.validation.ConstraintViolationException
+import org.springframework.http.HttpHeaders
+import org.springframework.http.HttpStatusCode
 import org.springframework.http.ResponseEntity
+import org.springframework.http.converter.HttpMessageNotReadableException
 import org.springframework.web.bind.annotation.ControllerAdvice
 import org.springframework.web.bind.annotation.ExceptionHandler
+import org.springframework.web.context.request.WebRequest
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler
 
 @ControllerAdvice
@@ -18,4 +22,13 @@ class ErrorHandler: ResponseEntityExceptionHandler() {
     
     @ExceptionHandler(Exception::class)
     fun fallback(e: Exception) = ResponseEntity.internalServerError().body("Failure: ${e.message}")
+
+    override fun handleHttpMessageNotReadable(ex: HttpMessageNotReadableException, headers: HttpHeaders, status: HttpStatusCode, request: WebRequest): ResponseEntity<in Any>? {
+        return ResponseEntity.badRequest().body(ErrorResponse(400, ex.message?: "Malformed request body"))
+    }
+
+    class ErrorResponse(
+        val status: Int,
+        val message: String
+    )
 }
