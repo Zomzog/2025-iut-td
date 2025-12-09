3ed7483b8cee547d488986f9bd861031539be3a4
Zomzog <zomzog@gmail.com>
2025-12-09 11:58:51 +0100

    Exo25

--- exo21/src/main/kotlin/iut/nantes/exo21/config/ErrorHandler.kt
+++ exo21/src/main/kotlin/iut/nantes/exo21/config/ErrorHandler.kt
@@ -1,5 +1,6 @@
 package iut.nantes.exo21.config
 
+import iut.nantes.exo21.errors.ImATeapotException
 import jakarta.validation.ConstraintViolationException
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.ControllerAdvice
@@ -11,6 +12,9 @@ class ErrorHandler: ResponseEntityExceptionHandler() {
 
     @ExceptionHandler(ConstraintViolationException::class)
     fun handleConstraintViolation(e: ConstraintViolationException) = ResponseEntity.badRequest().body("Failure: ${e.message}")
+
+    @ExceptionHandler(ImATeapotException::class)
+    fun handleImATeapot(e: ImATeapotException) = ResponseEntity.status(418).body("I'm a teapot !")
     
     @ExceptionHandler(Exception::class)
     fun fallback(e: Exception) = ResponseEntity.internalServerError().body("Failure: ${e.message}")
