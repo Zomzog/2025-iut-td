ea77778b7eff95a3dc2be9fe13c707c148c1c1c8
Zomzog <zomzog@gmail.com>
2025-12-09 13:43:54 +0100

    Exo35

--- exo33/src/main/kotlin/iut/nantes/exo33/DatabaseProxy.kt
+++ exo33/src/main/kotlin/iut/nantes/exo33/DatabaseProxy.kt
@@ -2,10 +2,12 @@ package iut.nantes.exo33
 
 import iut.nantes.exo33.controller.HumanDto
 import iut.nantes.exo33.controller.PetDto
+import iut.nantes.exo33.repository.HumanEntity
+import iut.nantes.exo33.repository.HumanRepository
 import org.springframework.stereotype.Service
 
 @Service
-class DatabaseProxy() {
+class DatabaseProxy(val repository: HumanRepository) {
     fun savePet(pet: PetDto): PetDto {
         TODO()
     }
@@ -19,14 +21,28 @@ class DatabaseProxy() {
     }
 
     fun saveHuman(humanDto: HumanDto): HumanDto {
-        TODO()
+        val saved = repository.save(HumanEntity(
+            humanId = humanDto.humanId,
+            name = humanDto.name
+        ))
+        return saved.toDto()
     }
 
     fun findHumanById(id: Int): HumanDto? {
-        TODO()
+       return repository.findById(id).map { it.toDto() }.orElse(null)
     }
 
     fun findAllHuman(): List<HumanDto> {
-        TODO()
+        return repository.findAll().map { it.toDto() }
     }
 }
+
+fun HumanDto.toEntity(): HumanEntity = HumanEntity(
+    humanId = this.humanId,
+    name = this.name
+)
+
+fun HumanEntity.toDto(): HumanDto = HumanDto(
+    humanId = this.humanId,
+    name = this.name
+)
