e68ac313bef9b49f53ff272bf0eab65d4b738144
Zomzog <zomzog@gmail.com>
2025-12-09 13:20:42 +0100

    Exo29

--- exo21/src/main/kotlin/iut/nantes/exo21/controller/InfoController.kt
+++ exo21/src/main/kotlin/iut/nantes/exo21/controller/InfoController.kt
@@ -7,24 +7,9 @@ import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
-class InfoController {
-    @Value("\${custom.app-name}")
-    private lateinit var appName: String
-    @Value("\${custom.app-version}")
-    private lateinit var appVersion: String
-    @Value("\${custom.git.branch}")
-    private lateinit var gitBranch: String
-    @Value("\${custom.git.commit}")
-    private lateinit var gitCommit: String
+class InfoController(val infoProperties: Info) {
     @GetMapping("/api/v1/info")
     fun getInfo(): Info {
-        return Info(
-            appName = appName,
-            appVersion = appVersion,
-            git = Git(
-                branch = gitBranch,
-                commit = gitCommit
-            )
-        )
+        return infoProperties
     }
 }
