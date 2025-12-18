70ca916ca2ed5b7e296275fba05e15b6f1ae982e
Zomzog <zomzog@gmail.com>
2025-12-18 09:51:06 +0100

    Exo44.5

--- exo33/src/main/kotlin/iut/nantes/exo33/config/SecurityConfig.kt
+++ exo33/src/main/kotlin/iut/nantes/exo33/config/SecurityConfig.kt
@@ -3,16 +3,20 @@ package iut.nantes.exo33.config
 import org.springframework.boot.webservices.client.WebServiceMessageSenderFactory.http
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.context.annotation.Profile
 import org.springframework.http.HttpMethod
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
 import org.springframework.security.web.SecurityFilterChain
 import org.springframework.security.config.annotation.web.invoke
 import org.springframework.security.core.userdetails.User
+import org.springframework.security.core.userdetails.UserDetailsService
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
 import org.springframework.security.crypto.password.PasswordEncoder
 import org.springframework.security.provisioning.InMemoryUserDetailsManager
+import org.springframework.security.provisioning.JdbcUserDetailsManager
 import org.springframework.security.provisioning.UserDetailsManager
+import javax.sql.DataSource
 
 
 @EnableWebSecurity
@@ -22,10 +26,11 @@ class SecurityConfig {
     fun filterChain(http: HttpSecurity): SecurityFilterChain {
         http {
             csrf { disable() }
+            headers { frameOptions { disable() } }
             authorizeHttpRequests {
                 authorize (HttpMethod.GET, "/**", permitAll)
                 authorize("/api/v1/humans/*/pets/*", hasRole("ADMIN"))
-                authorize(anyRequest, authenticated)
+                authorize(anyRequest, permitAll) // for h2-console
             }
             httpBasic { }
             formLogin { }
@@ -39,6 +44,7 @@ class SecurityConfig {
     }
 
     @Bean
+    @Profile("dev")
     fun userDetailService(passwordEncoder: PasswordEncoder): UserDetailsManager {
         val admin = User.withUsername("admin")
             .password(passwordEncoder.encode("1234"))
@@ -49,5 +55,18 @@ class SecurityConfig {
             .roles("ADMIN")
             .build()
         return InMemoryUserDetailsManager(admin, demo)
+    }
+
+    @Bean
+    @Profile("!dev")
+    fun jpaUserDetailService(dataSource: DataSource,
+                          passwordEncoder: PasswordEncoder): UserDetailsManager {
+        val user1 = User.withUsername("u1")
+            .password(passwordEncoder.encode("pw"))
+            .roles("USER")
+            .build()
+        return JdbcUserDetailsManager(dataSource).apply {
+            createUser(user1)
+        }
     }
 }
